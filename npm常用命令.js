1b10286a4acfa5a2599528b8b7c58c430d3029df
milk054 <67512290+milk054@users.noreply.github.com>
2020-12-27 21:17:57 +0800

    npm的常用命令

---
+++ npm常用命令.js
@@ -0,0 +1,18 @@
+/* 
+  npm init（初始化）
+  npm install [-D] 包名 [-g]（安装最新包）
+  npm install [-D] 包名@版本 [-g]（安装指定版本）
+  npm uninstall 包名 [-g]（卸载指定包）
+  npm list [--depth=0] [-g]（查询安装的包）
+  npm root [-g]（查询包的安装路径）
+  npm outdated（检查有哪些包需要更新）
+  npm updated [包名] [-g]（更新包）
+  npm info 包名（查询包的信息）
+  npm run 配置命令（运行package.json中配置的scripts字段）
+  npx cli命令（运行本地安装的cli命令）
+
+  npm config list [-l] [--json]（查询配置）
+  npm config get 配置项（获取某个配置项）
+  npm config set 配置项=值（设置某个配置项）
+  npm config delete 配置项（移除某个配置项）
+*/
