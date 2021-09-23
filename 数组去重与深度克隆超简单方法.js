30600a0f93d598537729d05459b01b624c73c67b
milk054 <67512290+milk054@users.noreply.github.com>
2021-09-23 10:24:41 +0800

    Delete 数组去重与深度克隆超简单方法.js

--- 数组去重与深度克隆超简单方法.js
+++
@@ -1,9 +0,0 @@
-// 数组去重
-function unique(arr, newArr = []) {
-  return newArr = [...new Set(arr)];
-}
-
-//深度克隆
-function deepClone(oldObj, newObj = {}) {
-  return newObj = JSON.parse(JSON.stringify(oldObj));
-}
