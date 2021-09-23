2af2377fc932c674b93be7c8aedb77fedf3a8683
milk054 <67512290+milk054@users.noreply.github.com>
2021-09-23 10:23:52 +0800

    Update 常用的工具函数.js

--- 常用的工具函数.js
+++ 常用的工具函数.js
@@ -1,3 +1,14 @@
+// 判断数据类型
 function dataType(data) {
   return Object.prototype.toString.call(data).match(/\[object (.*)\]/)[1].toLowerCase();
+}
+
+// 数组去重
+function uniqueArr(arr, newArr = []) {
+  return newArr = [...new Set(arr)];
+}
+
+//深度克隆对象
+function deepClone(oldObj, newObj = {}) {
+  return newObj = JSON.parse(JSON.stringify(oldObj));
 }
